1c96792e2cbdea72d75bd00b053536fe0cdd0ddb
Finaritra Andriambololona <antsarafanambinantsoa@gmail.com>
2026-03-15 13:39:57 +0100

    rectification forme

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 from traitement import modelisation
 
 app = FastAPI()
 
-
 @app.get("/")
 def direBonjour():
     return {"mot": "bonjour tous le monde"}
-
 
 @app.get("/modelisation")
 def modeliser(files):
--- traitement.py
+++ traitement.py
@@ -1,12 +1,12 @@
-## 0) Data engenging
-## 1) Preprocessing
-## 2) Standardisation
-## 3) Echantillonage( Cross-validation )
-## 4) Apprentissage
-## 5) Comparaison
-## 6) Interpretation
-## 7) Test de stabilité
-## 8) Choix du model
+# 0) Data engenging
+# 1) Preprocessing
+# 2) Standardisation
+# 3) Echantillonage( Cross-validation )
+# 4) Apprentissage
+# 5) Comparaison
+# 6) Interpretation
+# 7) Test de stabilité
+# 8) Choix du model
 
 import pandas as pd
 import sklearn
@@ -19,26 +19,28 @@ def modelisation(files):
     except FileNotFoundError:
         print("aucun fichier trouvé")
 
-    X = file[["heures_etude", "heures_sommeil", "presence_cours"]]
+    X = file[["heures_etude", 
+              "heures_sommeil", 
+              "presence_cours"]]
+    
     Y = file[["score"]]
 
     # ***************Ici nous n'allons utiliser que la regression lineaire multiple **************
     model = sklearn.linear_model.LinearRegression()
 
     # Validation croisée 5-fold
-    scores = sklearn.model_selection.cross_val_score(model, X, Y, cv=5)
+    scores = sklearn.model_selection.cross_val_score(model, 
+                                                     X,
+                                                     Y, 
+                                                     cv=5)
 
     # Entrainement final apre selection du model
     # Qui sur notre cas n'est pa necassaire
     model.fit(X, Y)
 
-    ## ici le valuer de retour est un json donc
-    return {"Scores par fold ": scores.tolist(), "Score moyen ": scores.mean()}
-
-
-print("salut")
-
-
+    # ici le valuer de retour est un json donc
+    return {"Scores par fold ": scores.tolist(),
+            "Score moyen ": scores.mean()}
 """"
 
 from sklearn.ensemble import RandomForestRegressor
